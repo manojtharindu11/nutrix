4e9a345efbb8432df9891f1e99a57f01ce8f54c0
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-12-03 01:32:17 +0530

    Configure the routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, SafeAreaView, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>This is starting page</Text>
+      <Link href={"/home"}>Home</Link>
+      <Link href={"/login"}>Login</Link>
+      <Link href={"/register"}>Register</Link>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
