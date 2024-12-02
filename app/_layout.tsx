e71803748a35fc9a79f878387f4b8146491148d1
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-12-03 01:31:38 +0530

    Configure the routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/home");
+  }, []);
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="register" options={{ headerShown: false }} />
+      <Stack.Screen name="home" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
