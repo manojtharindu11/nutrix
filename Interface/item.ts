cf6b66d5f9b38390bf9ef1a10cc99107d689b8e4
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-12-23 22:29:48 +0530

    Add Item interface and ItemWithReact extension for type safety

---
+++ Interface/item.ts
@@ -0,0 +1,16 @@
+interface Item {
+  nix_item_id: string;
+  food_name: string;
+  brand_name: string;
+  photo: { thumb: string };
+  serving_unit: string;
+  serving_qty: number;
+  nf_calories: number;
+  brand_name_item_name: string;
+}
+
+export default Item;
+
+interface ItemWithReact extends Item {
+    react: boolean;
+}
